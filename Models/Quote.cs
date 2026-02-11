427d4d14e9b4d38abba0becdfc431397f959c125
jasmine-codes <jsmlee97@gmail.com>
2026-02-11 12:43:46 +0100

    create models - quote

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -1,4 +1,9 @@
 namespace SimpsonsApiTests.Models
 {
-    
+    public class Quote
+    {
+        public string Quote { get; set; }
+        public string Character { get; set; }
+        public string Image { get; set; }
+    }
 }
