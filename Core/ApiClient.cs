c8cd832eaee0b67fee5208bd0582b2c04df85308
jasmine-codes <jsmlee97@gmail.com>
2026-02-12 10:52:08 +0100

    create API client

--- Core/ApiClient.cs
+++ Core/ApiClient.cs
@@ -1,2 +1,14 @@
 using RestSharp;
 using System.Threading.Tasks;
+
+namespace SimpsonsApiTests.Core
+{
+    public class ApiClient
+    {
+        private readonly RestClient _client;
+        public ApiClient(string baseUrl)
+        {
+            _client = new RestClient(baseUrl);
+        }
+    }
+}
