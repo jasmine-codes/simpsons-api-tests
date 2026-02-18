b757d502a22b3d4f559956c25e6ef1d9c3fcf1e3
jasmine-codes <jsmlee97@gmail.com>
2026-02-18 09:30:55 +0100

    add response assertions

--- Helpers/ResponseAssertions.cs
+++ Helpers/ResponseAssertions.cs
@@ -10,5 +10,10 @@ namespace SimpsonsApiTests.Helpers
             Assert.That((int)response.StatusCode, Is.EqualTo(expected),
             $"Expected status {expected} but got {(int)response.StatusCode}");
         }
+
+        public static void AssertNotEmpty(string? value, string fieldName)
+        {
+            
+        }
     }
 }
