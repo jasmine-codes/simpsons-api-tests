38e322ee0982083a51956d5b3b6d02f01a2d86db
jasmine-codes <jsmlee97@gmail.com>
2026-02-19 10:47:41 +0100

    add test logger

--- Helpers/TestLogger.cs
+++ Helpers/TestLogger.cs
@@ -3,5 +3,11 @@ using System;
 
 namespace SimpsonsApiTests.Helpers
 {
-    
+    public static class TestLogger
+    {
+        public static void LogResponse(RestResponse response)
+        {
+            
+        }
+    }
 }
