107bfadabfd2a0e31f1654326b63fe84cfc00963
jasmine-codes <jsmlee97@gmail.com>
2026-02-19 10:50:11 +0100

    add test logger

--- Helpers/TestLogger.cs
+++ Helpers/TestLogger.cs
@@ -7,7 +7,13 @@ namespace SimpsonsApiTests.Helpers
     {
         public static void LogResponse(RestResponse response)
         {
-            
+            Console.WriteLine("---RESPONSE---");
+
+            Console.WriteLine($"Status: {(int)response.StatusCode}");
+
+            Console.WriteLine(response.Content);
+
+            Console.WriteLine("----------------");
         }
     }
 }
